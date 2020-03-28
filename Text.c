b46262570a00e750a25a1ca3dcc644a99b43cfcd
gduthai <44573787+gduthai@users.noreply.github.com>
2020-03-28 21:39:53 +0800

    Update Text.c

---
+++ Text.c
@@ -0,0 +1,59 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+typedef struct number {
+	int mu;  //保存分子
+	int zi;     //保存分母
+}Number;
+
+
+int gcd(int a, int b){                          //求最大公约数
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+
+
+int lcm(int a, int b) {                         //求最小公倍数：相乘除以最大公约数
+	return a * b / gcd(a, b);                
+}
+
+void symbolize(Number *num) {                        //分数化简
+	int n = gcd(num->zi, num->mu);
+	num->zi /= n;                                               //上下除以最大公约数即可化简
+	num->mu /= n;
+}                          
+
+
+void calculate(char signal, Number *left, Number *right) {  // 计算函数并化简，结果保存在左边的数中
+	int n,m,z;
+	n = lcm(left->mu, right->mu);
+	m = gcd(left->mu, right->mu);
+	if (signal == '+') {
+		z = left->zi * right->mu / m + right->zi * left->mu / m;  //求分子
+		left->mu = n;
+		left->zi = z;
+		symbolize(left);
+	}
+	if (signal == '-') {
+		z = left->zi * right->mu / m - right->zi * left->mu / m;  //求分子
+		left->mu = n;
+		left->zi = z;
+		symbolize(left);
+	}
+	if (signal == '*') {
+		left->mu = left->mu * right->mu;
+		left->zi = left->zi * right->zi;
+		symbolize(left);
+	}
+	if (signal == '/') {
+		left->mu = left->mu * right->zi;
+		left->zi = left->zi * right->mu;
+		symbolize(left);
+	}
+}
+
+void main() {
+	Number num1 = { 1,2 }, num2 = { 3 , 2 };
+	calculate('+', &num1, &num2);
+	printf("%d/%d", num1.zi.num1.mu);
+}
+
